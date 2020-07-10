40a0c336e032d4f868b2a5349f756fabe63ce805
Spenser Andreassen <spenserca@gmail.com>
2020-07-10 14:09:43 -0500

    refactor: move test class instantiation up

--- src/decorators/scrubbable.spec.ts
+++ src/decorators/scrubbable.spec.ts
@@ -10,49 +10,50 @@ const scrubToJSONMock = scrubToJSON as jest.Mock;
 @scrubbable
 class TestClass extends Object {}
 
+let testClass: any;
+
+beforeEach(() => {
+  testClass = new TestClass();
+});
+
 describe('when getting the string value of the object', () => {
   let actual: any;
-  let testClass: TestClass;
   let expected: string;
 
   beforeEach(() => {
     expected = chance.string();
     scrubToStringMock.mockReturnValue(expected);
 
-    testClass = new TestClass();
     actual = testClass.toString();
   });
 
-  it('gets the string representation of the object', () => {
+  it('gets the scrubbed string representation of the object', () => {
     expect(scrubToStringMock).toHaveBeenCalledTimes(1);
     expect(scrubToStringMock).toHaveBeenCalledWith(testClass);
   });
 
-  it('returns the string representation of the object', () => {
+  it('returns the scrubbed string representation of the object', () => {
     expect(actual).toEqual(expected);
   });
 });
 
 describe('when getting the json value of the object', () => {
   let actual: any;
-  let testClass: TestClass;
   let expected: any;
 
   beforeEach(() => {
     expected = { [chance.string()]: chance.string() };
     scrubToJSONMock.mockReturnValue(expected);
 
-    testClass = new TestClass();
-    // @ts-ignore
     actual = testClass.toJSON();
   });
 
-  it('gets the string representation of the object', () => {
+  it('gets the scrubbed string representation of the object', () => {
     expect(scrubToJSONMock).toHaveBeenCalledTimes(1);
     expect(scrubToJSONMock).toHaveBeenCalledWith(testClass);
   });
 
-  it('returns the string representation of the object', () => {
+  it('returns the scrubbed json representation of the object', () => {
     expect(actual).toEqual(expected);
   });
 });
